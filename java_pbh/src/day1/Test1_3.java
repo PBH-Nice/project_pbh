72b0e29f235c08a7279852b3c9801daf07ea7af0
PBH-Nice <69798504+PBH-Nice@users.noreply.github.com>
2021-04-26 17:37:45 +0900

    자바 1일차 : 테스트 예제

---
+++ java_pbh/src/day1/Test1_3.java
@@ -0,0 +1,46 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Test1_3 {
+
+	public static void main(String[] args) {
+		// 두 정수와 문자를 입력받아 출력하는 예제를 작성하세요.
+		/* 예시1
+		 * 정수1을 입력하세요 : 1
+		 * 문자를 입력 하세요: +
+		 * 정수2를 입력 하세요 : 2
+		 * 1 + 2
+		 * 예시
+		 * 두 정수와 문자를 입력하세요(예: 1 + 2) : 1 + 2
+		 * 1 + 2
+		 */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정수1을 입력하세요 : ");
+		int num1 = scan.nextInt();
+		
+		System.out.print("문자를 입력하세요 : ");
+		char ch = scan.next().charAt(0);
+		
+		System.out.print("정수2를 입력하세요 : ");
+		int num2 = scan.nextInt();
+		System.out.println(""+num1 + ch + num2);
+		
+		System.out.print("두 정수와 문자를 입력하세요 (예: 1 + 2) ");
+		int num3 = scan.nextInt();
+		char ch1 = scan.next().charAt(0);
+		int num4 = scan.nextInt();
+		System.out.println(""+num1 + ch + num2);
+		
+		scan.close();
+				
+
+		
+		
+		
+
+	}
+
+}
